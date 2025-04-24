f91cb38a3cdf26f671285a2c36056878edf9d25d
Adhitha Dias <kadhitha@purdue.edu>
2025-04-23 22:40:03 -0400

    update with triton results and generate new plots

--- scripts/bz.py
+++ scripts/bz.py
@@ -24,6 +24,7 @@ def plot_grouped_bars_with_speedup(data, title, output_file, throughput : bool =
     cuda1 = data['cuda1(us)']
     cuda2 = data['cuda2(us)']
     cuda3 = data['cuda3(us)']
+    triton = data['triton(us)']
     FLOPs = data['FLOPs']
     
     if throughput:
@@ -31,43 +32,49 @@ def plot_grouped_bars_with_speedup(data, title, output_file, throughput : bool =
         cuda1 = FLOPs / cuda1 * 10**6 / 10**9
         cuda2 = FLOPs / cuda2 * 10**6 / 10**9
         cuda3 = FLOPs / cuda3 * 10**6 / 10**9
+        triton = FLOPs / triton * 10**6 / 10**9
 
     # Calculate speedup over default
     if throughput:
         speedup_cuda1 = cuda1 / default 
         speedup_cuda2 = cuda2 / default
         speedup_cuda3 = cuda3 / default
+        triton_speedup = triton / default
         geomean_cuda1 = np.exp(np.mean(np.log(speedup_cuda1)))
         geomean_cuda2 = np.exp(np.mean(np.log(speedup_cuda2)))
         geomean_cuda3 = np.exp(np.mean(np.log(speedup_cuda3)))
+        geomean_triton = np.exp(np.mean(np.log(triton_speedup)))
     else:
         speedup_cuda1 = default / cuda1
         speedup_cuda2 = default / cuda2
         speedup_cuda3 = default / cuda3
+        triton_speedup = default / triton
         geomean_cuda1 = np.exp(np.mean(np.log(speedup_cuda1)))
         geomean_cuda2 = np.exp(np.mean(np.log(speedup_cuda2)))
         geomean_cuda3 = np.exp(np.mean(np.log(speedup_cuda3)))
+        geomean_triton = np.exp(np.mean(np.log(triton_speedup)))
 
     x = np.arange(len(labels))  # Label locations
-    width = 0.20  # Width of the bars
+    width = 0.15  # Width of the bars
 
     fig, ax1 = plt.subplots(figsize=(10, 6))
 
     # Bar plots for execution times
-    ax1.bar(x - 1.5*width, default, width, label='Baseline', color='lightskyblue')
-    ax1.bar(x - 0.5*width, cuda1, width, label='CUDA1', color='lightcoral')
+    ax1.bar(x - 2*width, default, width, label='Baseline', color='lightskyblue')
+    ax1.bar(x - 1*width, cuda1, width, label='CUDA1', color='lightcoral')
     # set ax1.bar color to light orange
-    ax1.bar(x + 0.5*width, cuda2, width, label='CUDA2', color='lightgreen')
+    ax1.bar(x + 0*width, cuda2, width, label='CUDA2', color='lightgreen')
     # ax1.bar(x + 0.5*width, cuda2, width, label='CUDA2', color='green')
     # set ax1.bar color to light purple
-    ax1.bar(x + 1.5*width, cuda3, width, label='CUDA3', color='violet')
+    ax1.bar(x + 1*width, cuda3, width, label='CUDA3', color='violet')
+    ax1.bar(x + 2*width, triton, width, label='Triton', color='lightgray')
 
     ax1.set_xlabel('Batch Size (b)', fontsize=18)
     if throughput:
         ax1.set_ylabel('Throughput (GFLOPS)', fontsize=18)
     else:
         ax1.set_ylabel('Time (us)', fontsize=18)
-    ax1.set_title(f"{title}, Geomean Speedup: {geomean_cuda2:.2f}", fontsize=22)
+    ax1.set_title(f"{title}, GM CUDA2: {geomean_cuda2:.2f}, GM Triton: {geomean_triton:.2f}", fontsize=22)
     ax1.set_xticks(x)
     ax1.set_xticklabels(labels, fontsize=18)
     ax1.tick_params(axis='y', labelsize=16)
@@ -79,12 +86,13 @@ def plot_grouped_bars_with_speedup(data, title, output_file, throughput : bool =
     ax2.plot(x, speedup_cuda1, label='Speedup CUDA1', color='red', marker='o', linestyle='--')
     ax2.plot(x, speedup_cuda2, label='Speedup CUDA2', color='green', marker='o', linestyle='--')
     ax2.plot(x, speedup_cuda3, label='Speedup CUDA3', color='purple', marker='o', linestyle='--')
+    ax2.plot(x, triton_speedup, label='Speedup Triton', color='black', marker='o', linestyle='--')
     ax2.set_ylabel('Speedup over Baseline', fontsize=18)
-    ax2.legend(loc='upper right', fontsize=14)
+    ax2.legend(loc='lower right', fontsize=14)
     
     # limit secondary y-axis to 0.9 to 1.3
-    ax2.set_ylim(0.8, 1.2)
-    ax2.set_yticks(np.arange(0.8, 1.3, 0.1))
+    ax2.set_ylim(0.3, 1.3)
+    ax2.set_yticks(np.arange(0.3, 1.4, 0.1))
     
     # set y ticks fontsize to 18
     ax2.tick_params(axis='y', labelsize=16)
