a38506ef656e3193d86b358e11e36b284bb06bfc
Praditha Alwis <alwispraditha20@gmail.com>
2025-05-06 12:14:00 -0400

    Added the triton implementation 2

--- rotary_embedding.py
+++ rotary_embedding.py
@@ -6,6 +6,7 @@ import time
 
 import rotary_emb
 from rotary_embedding_triton import apply_rotary
+from rotary_embedding_triton_complex import apply_rotary_direct_theta
 import torch
 from einops import rearrange, repeat
 
@@ -303,12 +304,74 @@ class ApplyRotaryEmbTriton(torch.autograd.Function):
             dx[..., rotary_dim:].copy_(do[..., rotary_dim:])
         return dx, None, None, None, None
 
+class ApplyRotaryEmbTriton2(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, x, theta, interleaved=False, inplace=False):
+        """
+            x: (batch_size, seqlen, nheads, headdim)
+            cos, sin: (seqlen, rotary_dim / 2)
+            interleaved: if True, rotate pairs of even and odd dimensions (GPT-J style) instead
+                of 1st half and 2nd half (GPT-NeoX style).
+        rotary_dim must be <= headdim
+        Apply rotary embedding to the first rotary_dim of x.
+        """
+        batch, seqlen, nheads, headdim = x.shape
+        rotary_seqlen, rotary_dim = theta.shape
+        rotary_dim *= 2
+        assert rotary_dim <= headdim
+        assert seqlen <= rotary_seqlen
+        assert theta.shape == (rotary_seqlen, rotary_dim // 2)
+        x = x[..., :rotary_dim]
+        out = torch.empty_like(x) if not inplace else x
+        # print('x.shape:', x.shape)
+        # print('x1.shape:', x1.shape)
+        # print('x2.shape:', x2.shape)
+        # print('cos shape:', cos[:seqlen].shape)
+        # print('cos[:seqlen].shape:', cos[:seqlen].shape)
+        # print('sin[:seqlen].shape:', sin[:seqlen].shape)
+        # print('o1.shape:', o1.shape)
+        # print('o2.shape:', o2.shape)
+        x = x.contiguous()
+        apply_rotary_direct_theta(
+            x,
+            theta,
+            out,
+            False,
+        )
+        # print('o1\n', out)
+        if not inplace and rotary_dim < headdim:
+            out[..., rotary_dim:].copy_(x[..., rotary_dim:])
+        ctx.save_for_backward(theta)
+        ctx.interleaved = interleaved
+        ctx.inplace = inplace
+        return out if not inplace else x
+
+    @staticmethod
+    def backward(ctx, do):
+        theta, = ctx.saved_tensors
+        _, seqlen, _, headdim = do.shape
+        rotary_dim = theta.shape[-1]
+        # rotary_dim *= 2
+        inplace = ctx.inplace
+        # do_ro = do[..., :rotary_dim]
+        dx = torch.empty_like(do) if not inplace else do
+        do = do.contiguous()
+        apply_rotary_direct_theta(
+            do,
+            theta,
+            dx,
+            True,
+        )
+        if not inplace and rotary_dim < headdim:
+            dx[..., rotary_dim:].copy_(do[..., rotary_dim:])
+        return dx, None, None, None, None
 
 
-apply_rotary_emb_func = ApplyRotaryEmb.apply
+# apply_rotary_emb_func = ApplyRotaryEmb.apply
 apply_rotary_emb_func2 = ApplyRotaryEmb2.apply
-apply_rotary_emb_func3 = ApplyRotaryEmb3.apply
-apply_rotary_emb_triton = ApplyRotaryEmbTriton.apply
+# apply_rotary_emb_func3 = ApplyRotaryEmb3.apply
+# apply_rotary_emb_triton = ApplyRotaryEmbTriton.apply
+# apply_rotary_emb_triton2 = ApplyRotaryEmbTriton2.apply
 
 # def apply_rotary_emb_func(x, cos, sin, interleaved=False, inplace=False):
 #         """
--- rotary_embedding_triton.py
+++ rotary_embedding_triton.py
@@ -8,10 +8,10 @@ def rotary_kernel(
     cos_ptr,         # pointer to cos
     sin_ptr,         # pointer to sin
     out_ptr,         # pointer to output tensor
-    conj,            # conjugate flag
     B, S, H, D,      # batch, sequence, heads, dimension                #2, 2, 2, 4
     stride_bs, stride_sh, stride_hd,  # strides                         #16, 8, 4
-    BLOCK_D: tl.constexpr # optimize the kernel assuming it is fixed.
+    BLOCK_D: tl.constexpr, # optimize the kernel assuming it is fixed.
+    conj: tl.constexpr # conjugate flag
 ):
     
     # Triton allows launching a grid of programs in multiple dimensions.
@@ -21,65 +21,38 @@ def rotary_kernel(
 
     # [0, 1, 2, 3]
     offs_d = tl.arange(0, BLOCK_D)  # Vector of offsets for the feature dimension.
-    mask = offs_d < D               # Handle tail fragments when D is not a multiple of BLOCK_D
 
-    
-    # x_offset = b * stride_bs + s * stride_sh + h * stride_hd + offs_d   # flattened index
     x_offset = b * stride_bs + s * stride_sh + h * stride_hd   # flattened index
-    # x = tl.load(x_ptr + x_offset, mask=mask)
 
     # Cos/sin are [S, D//2]
     cos_offset = s * (D // 2) + offs_d
-    mask_half = offs_d < (D // 2)
+    mask_half = offs_d < (D // 2)  # Handle tail fragments when D is not a multiple of BLOCK_D
     cos = tl.load(cos_ptr + cos_offset, mask=mask_half, other=0.0)
     sin = tl.load(sin_ptr + cos_offset, mask=mask_half, other=0.0)
-    # cos = tl.load(cos_ptr + s * (BLOCK_D//2) + offs_d, mask=mask)
-    # sin = tl.load(sin_ptr + s * (BLOCK_D//2) + offs_d, mask=mask)
 
-    # x1 = x[::2]     # Even Values
-    # x2 = x[1::2]    # Odd Values
     offs_even = 2 * offs_d  # [0, 2, 4, 6]
-    x1 = tl.load(x_ptr + offs_even, mask=offs_even < D)     # Even Values
+    x_even = tl.load(x_ptr + offs_even, mask=offs_even < D)     # Even Values
 
     offs_odd = 2 * offs_d + 1   # [1, 3, 5, 7]
-    x2 = tl.load(x_ptr + offs_odd, mask=offs_odd < D)       # Odd Values
+    x_odd = tl.load(x_ptr + offs_odd, mask=offs_odd < D)       # Odd Values
 
-    # offs = tl.arange(0, BLOCK_D // 2)  # NOT BLOCK_D
-    # offsets_even = offs * 2
-    # offsets_odd = offsets_even + 1
-
-    # x1 = tl.load(x_ptr + offsets_even, ...)   # shape: [BLOCK_D // 2]
-    # x2 = tl.load(x_ptr + offsets_odd, ...)    # shape: [BLOCK_D // 2]
-
-    # cos = tl.load(cos_ptr + s * (D // 2) + offs, ...)  # shape: [BLOCK_D // 2]
-    # sin = tl.load(sin_ptr + s * (D // 2) + offs, ...)  # shape: [BLOCK_D // 2]
-
-    # Triton does not support complex numbers natively, so we'll have to use the real-img approach
+    # Triton does not support complex numbers natively, so we'll have to use the sin-cos approach
     if not conj:        # Forward pass
-        x_rotated_even = x1 * cos - x2 * sin
-        x_rotated_odd = x1 * sin + x2 * cos
+        x_rotated_even = x_even * cos - x_odd * sin
+        x_rotated_odd = x_even * sin + x_odd * cos
     else:               # Backward pass
-        x_rotated_even = x1 * cos + x2 * sin
-        x_rotated_odd = -x1 * sin + x2 * cos
+        x_rotated_even = x_even * cos + x_odd * sin
+        x_rotated_odd = -x_even * sin + x_odd * cos
 
-    # # Interleave
-    # out = tl.zeros([BLOCK_D], dtype=tl.float32)
-    # out[::2] = x_rotated_even
-    # out[1::2] = x_rotated_odd
-
-    # Interleaving positions
+    # Interleave
     offs = tl.arange(0, BLOCK_D)
-    offs_even = offs * 2
-    offs_odd  = offs * 2 + 1
-    x_offset = b * stride_bs + s * stride_sh + h * stride_hd
+    offs_even = offs_d * 2
+    offs_odd  = offs_d * 2 + 1
     # x_offset = x_offset + tl.zeros([BLOCK_D // 2], dtype=tl.int32)  # shape match!
 
     # Compute final store addresses
     store_ptr_even = out_ptr + x_offset + offs_even
     store_ptr_odd  = out_ptr + x_offset + offs_odd
-
-    # tl.store(out_ptr + x_offset + offs_even, x_rotated_even)
-    # tl.store(out_ptr + x_offset + offs_odd,  x_rotated_odd)
 
     # Store interleaved output
     mask_even = offs_even < D
@@ -87,11 +60,6 @@ def rotary_kernel(
     tl.store(store_ptr_even, x_rotated_even, mask=mask_even)
     tl.store(store_ptr_odd,  x_rotated_odd,  mask=mask_odd)
 
-    # tl.store(store_ptr_even, x_rotated_even)
-    # tl.store(store_ptr_odd,  x_rotated_odd)
-
-
-    # tl.store(out_ptr + x_offset, out, mask=mask)
 
 
 # def rotary_kernel(
@@ -149,9 +117,10 @@ def apply_rotary(x, cos, sin, out=None, conj=False):
     # assert x.dtype == cos.dtype == sin.dtype
     grid = (B, S, H)
     rotary_kernel[grid](
-        x, cos, sin, out, conj,
+        x, cos, sin, out,
         B, S, H, D,
         x.stride(0), x.stride(1), x.stride(2),
-        BLOCK_D=triton.next_power_of_2(D)       # Set the feature dimension to a power of 2
+        BLOCK_D=triton.next_power_of_2(D),       # Set the feature dimension to a power of 2
+        conj=conj
     )
     return out
